bb0d74b5959a65c12be9d4331792c5a8fb0519ee
gadielpaiz <gadiel.paiz@gmail.com>
2024-01-23 19:48:27 -0600

    CAL-1: Done

---
+++ CalGads/CalGads/CalGadsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalGadsApp.swift
+//  CalGads
+//
+//  Created by Gadiel Paiz on 1/23/24.
+//
+
+import SwiftUI
+
+@main
+struct CalGadsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
